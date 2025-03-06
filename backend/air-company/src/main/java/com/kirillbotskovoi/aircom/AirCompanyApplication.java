c80f71844e09b2d417b61c501c8d1bf7ba401a1a
kirill-botskovoi <kirill.botskovoi@gmail.com>
2025-03-06 20:04:12 +0200

    created base for application
    added project structure, dependencies

---
+++ backend/air-company/src/main/java/com/kirillbotskovoi/aircom/AirCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.kirillbotskovoi.aircom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirCompanyApplication.class, args);
+    }
+
+}
